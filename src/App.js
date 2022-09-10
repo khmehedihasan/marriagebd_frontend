fcb469f4f12b447e82a1ca803948f08cd1361186
khmehedihasan <mehedihasan47341@gmail.com>
2022-09-10 18:21:51 +0600

    update

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
 import Login from './pages/Login';
+import Signup from './pages/signup/Signup';
+import Step2 from './pages/signup/Step2';
+import Step3 from './pages/signup/Step3';
 
 function App() {
   firstLoad();
@@ -14,6 +17,9 @@ function App() {
         <Route path='/about' element={<About />} />
         <Route path='/contact' element={<Contact />} />
         <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/signup/step_2' element={<Step2 />} />
+        <Route path='/signup/step_3' element={<Step3 />} />
     </Routes>
   );
 }
