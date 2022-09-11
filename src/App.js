2c51ede1e78907bbef55a78ffb0f6e1bd899f754
khmehedihasan <mehedihasan47341@gmail.com>
2022-09-11 12:53:04 +0600

    update

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import Signup from './pages/signup/Signup';
 import Step2 from './pages/signup/Step2';
 import Step3 from './pages/signup/Step3';
 import Step4 from './pages/signup/Step4';
+import Step5 from './pages/signup/Step5';
+import Step6 from './pages/signup/Step6';
 
 function App() {
   firstLoad();
@@ -22,6 +24,8 @@ function App() {
         <Route path='/signup/step_2' element={<Step2 />} />
         <Route path='/signup/step_3' element={<Step3 />} />
         <Route path='/signup/step_4' element={<Step4 />} />
+        <Route path='/signup/step_5' element={<Step5 />} />
+        <Route path='/signup/step_6' element={<Step6 />} />
     </Routes>
   );
 }
