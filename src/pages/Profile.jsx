41608e563a220e820cdcd4e3e11c1a156c2d8558
khmehedihasan <mehedihasan47341@gmail.com>
2022-09-14 18:10:02 +0600

    update

---
+++ src/pages/Profile.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Footer from '../components/Footer';
+import NavHome from '../components/NavHome';
+
+function Profle(){
+    return(
+        <>
+            <NavHome />
+            <div className=" w-full h-auto mt-16 bg-red-100">
+
+            </div>
+            <Footer />
+        </>
+    )
+}
+
+export default Profle;
