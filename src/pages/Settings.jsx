466be690d0a00982035ac0ee0eaf058354f87b7a
khmehedihasan <mehedihasan47341@gmail.com>
2022-09-14 18:10:18 +0600

    added

---
+++ src/pages/Settings.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import NavHome from '../components/NavHome';
+
+function Settings(){
+    return(
+        <>
+          <NavHome />  
+            <div className=" w-full h-auto mt-16 bg-red-100">
+
+            </div>
+        </>
+    )
+}
+
+export default Settings;
