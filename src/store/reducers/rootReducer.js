212464006df26eb0ebdf260d8a013b6b15d6fb2d
khmehedihasan <mehedihasan47341@gmail.com>
2022-09-19 19:00:54 +0600

    update

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -1,8 +1,14 @@
 import { combineReducers } from "redux";
 import filter from "./filter";
+import home_division from "./home_division";
+import education from "./education";
+import working_sector from "./working_sector";
 
 const rootReducer = combineReducers({
     filter,
+    home_division,
+    education,
+    working_sector
 });
 
 export default rootReducer;
