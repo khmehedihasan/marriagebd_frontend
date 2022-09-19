f786256386ac02d8c81bceac2a1aa927a2238581
khmehedihasan <mehedihasan47341@gmail.com>
2022-09-19 19:00:10 +0600

    update

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,13 +1,43 @@
 import React from 'react';
+import BgContainer from '../components/BgContainer';
 import NavHome from '../components/NavHome';
+import Footer from '../components/Footer';
+import { Link } from 'react-router-dom';
+import { useCookies } from 'react-cookie';
+import useAuth from '../hooks/useAuth';
+import url from '../url';
+import { useNavigate } from 'react-router-dom';
 
 function Settings(){
+    const [cookies, setCookie, removeCookie] = useCookies(['auth']);
+    const {id} = useAuth();
+    const navigate = useNavigate();
+
+
+    function logOut(){
+        removeCookie('user',[{expires: Date.now()}]);     
+        
+        console.log(id)
+
+        fetch(`${url}/logout/${id}`,{method:'DELETE',mode:'cors',credentials:'include'}).then((data)=>data.json()).then((data)=>{
+            console.log(data)
+            navigate('/logIn');
+        });
+    }
     return(
         <>
-          <NavHome />  
-            <div className=" w-full h-auto mt-16 bg-red-100">
-
-            </div>
+            <NavHome /> 
+            <BgContainer>
+                <div className=' w-max md:w-[700px]  h-full mx-auto bg-slate-100 py-10 px-2 m-2'>
+                    <div className=' flex flex-col gap-5 px-6 py-10'>
+                        <Link to='/' className=" py-[2px] bg-sky-600 text-white text-lg md:text-2xl hover:text-sky-600 hover:bg-slate-100 rounded-md px-5"><i className="fa-solid fa-image"></i> Change Profile Picture</Link>
+                        <Link to='/' className=" py-[2px] bg-green-600 text-white text-lg md:text-2xl hover:text-green-600 hover:bg-slate-100 rounded-md px-5"><i className="fa-solid fa-address-card"></i> Change Info</Link>
+                        <Link to='/login/forgotPassword' className=" py-[2px] bg-teal-600 text-white text-lg md:text-2xl hover:text-teal-600 hover:bg-slate-100 rounded-md px-5"><i className="fa-solid fa-lock"></i> ChangePassword</Link>
+                        <button onClick={logOut} className=" py-[2px] bg-red-600 text-white text-left text-lg md:text-2xl hover:text-red-600 hover:bg-slate-100 rounded-md px-5"><i className="fa-solid fa-power-off"></i> Log Out</button>
+                    </div>
+                </div>
+            </BgContainer>
+            <Footer />
         </>
     )
 }
