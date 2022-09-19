72edce69f58fd158e9e4a9a0bb5ccf312fe191ea
khmehedihasan <mehedihasan47341@gmail.com>
2022-09-19 18:55:34 +0600

    update

---
+++ src/store/actions/search.js
@@ -0,0 +1,21 @@
+export function GET_CITY(data){
+    return({type:"GET_CITY", data});
+}
+export function GET_CITY_ALL(data){
+    return({type:"GET_CITY_ALL", data});
+}
+
+
+export function GET_EDU(data){
+    return({type:"GET_EDU", data});
+}
+export function GET_EDU_ALL(data){
+    return({type:"GET_EDU_ALL", data});
+}
+
+export function GET_WS(data){
+    return({type:"GET_WS", data});
+}
+export function GET_WS_ALL(data){
+    return({type:"GET_WS_ALL", data});
+}
