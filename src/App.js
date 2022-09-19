8f4e72fb5df566f99dbcb00f12aa0f87da522af5
khmehedihasan <mehedihasan47341@gmail.com>
2022-09-19 18:57:53 +0600

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import firstLoad from './hooks/firstLoad';
-import Index from './pages/Index';
+import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
+import Search from './pages/Search';
+
 import Login from './pages/login/Login';
 import Signup from './pages/signup/Signup';
 import Step2 from './pages/signup/Step2';
@@ -15,6 +17,7 @@ import Step7 from './pages/signup/Step7';
 import Verify from './pages/signup/Verify';
 import Forgot from './pages/login/Forgot';
 import Verify2 from './pages/login/Verify';
+
 import NewPassword from './pages/login/NewPassword';
 import IsLogIn from './pages/login/IsLogIn';
 import IsLogOut from './pages/login/IsLogOut';
@@ -26,9 +29,14 @@ function App() {
   firstLoad();
   return (
     <Routes>
-        <Route path='/' element={<Index />} />
+        <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
         <Route path='/contact' element={<Contact />} />
+        <Route path='/search' element={<Search />} />
+
+        <Route path='/login/forgotPassword' element={<Forgot />} />
+        <Route path='/login/code/:id' element={<Verify2 />} />
+        <Route path='/login/newPassword/:token/:id' element={<NewPassword />} />
 
         <Route path='/' element={<IsLogOut />}>
           <Route path='/login' element={<Login />} />
@@ -40,9 +48,6 @@ function App() {
           <Route path='/signup/step_6' element={<Step6 />} />
           <Route path='/signup/step_7' element={<Step7 />} />
           <Route path='/signup/verify/:id' element={<Verify />} />
-          <Route path='/login/forgotPassword' element={<Forgot />} />
-          <Route path='/login/code/:id' element={<Verify2 />} />
-          <Route path='/login/newPassword/:token/:id' element={<NewPassword />} />
         </Route>
 
         <Route path='/' element={<IsLogIn />}>
